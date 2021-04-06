5adc0e89b9b30d64ffe892ece5afd5b6ed450300
vovakrylenko <vovakrylenko@gmail.com>
2021-04-06 14:25:15 +0300

    Fix node_modules dependencies.

---
+++ typescript/hello-world/drawCats.js
@@ -0,0 +1,14 @@
+const drawCats = function (howManyTimes) {
+	for (var i = 0; i < howManyTimes; i++) {
+		console.log(i + " =^.^=");
+	}
+};
+drawCats(10);
+
+var rawCats1 = function (howMany) {
+	for (var i = 0; i < howMany; i++) {
+		console.log(i + " =^.^=");
+
+	}
+};
+drawCats(10);
